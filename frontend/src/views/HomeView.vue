7aa78bee40e8964b71fd8f129caefaed7ef48b8a
Andrew Vdovichenko <andrew.vdovichenko@gmail.com>
2023-03-12 07:07:58 +0500

    1.16.2 - сверстали шапку доски задач

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <main class="content">
+    <section class="desk">
+
+      <!-- Шапка доски -->
+      <div class="desk__header">
+        <h1 class="desk__title">Design Coffee Lab</h1>
+        <div class="desk__filters">
+          <div class="desk__user-filter">
+
+            <!-- Список пользователей -->
+            <ul class="user-filter">
+              <li class="user-filter__item">
+                <a class="user-filter__button">
+                  <img src="" alt="Аватар юзера" width="24" height="24">
+                </a>
+              </li>
+            </ul>
+
+          </div>
+          <div class="desk__meta-filter">
+
+            <!-- Список статусов -->
+            <ul class="meta-filter">
+              <li class="meta-filter__item">
+                <a class="meta-filter__stat" />
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Колонки и задачи -->
+      <!-- Пустая доска -->
+    </section>
+  </main>
+</template>
